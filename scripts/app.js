86cdcec3d979eacad80582097adc8137696429ad
fernandog301 <fernandog301g@gmail.com>
2023-12-04 17:46:09 -0800

    test

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,23 @@
+navigator.geolocation.getCurrentPosition(success, errorFunc);
+
+{
+  coords:{
+    latitude :32.1234;
+    longitude: 13.1234;
+  }
+}
+
+
+function success(position){
+  console.log("our Latitude: " + position.coords.latitude);
+  console.log("our longitude: "+ position.coords.longitude);
+}
+
+function errorFunc(error){
+  console.log(error.message);
+}
+
+
 let searchInput = document.getElementById("searchInput");
 let searchBtn = document.getElementById("searchBtn");
 let cityName = document.getElementById
@@ -5,6 +25,7 @@ let cityName = document.getElementById
 const apiKey = "f86e3df09941ea6ed9ee289a745f4331";
 const apiCurrentUrl = "https://openweathermap.org/current#name"; 
 const api5NameUrl = "https://openweathermap.org/current#name";
+
 
 async function searchWeather(city){
     try{
@@ -22,29 +43,33 @@ async function logMovies() {
 
 
 
-handleSubmit(){
-    var city = this.state.city;
-    var url = api5NameUrl+ apiKey + city;
-    console.log(url);
-    fetch(url, {
-      method: 'GET',
-      mode: "cors"
-    }).then((res) => {
-      var resJson = JSON.parse(res);
-      console.log(resJson);
-      return resJson;
-    }).then((resJson) => {
 
-    }).catch((err) => {
-      console.log("error");
-    });
-  }
+
+
+
+// handleSubmit(){
+//     var city = this.state.city;
+//     var url = api5NameUrl+ apiKey + city;
+//     console.log(url);
+//     fetch(url, {
+//       method: 'GET',
+//       mode: "cors"
+//     }).then((res) => {
+//       var resJson = JSON.parse(res);
+//       console.log(resJson);
+//       return resJson;
+//     }).then((resJson) => {
+
+//     }).catch((err) => {
+//       console.log("error");
+//     });
+//   }
   
-searchBtn.addEventListener('click', function (e){
-    let value = e.target.value
-    if(value && value.trim().l)
+// searchBtn.addEventListener('click', function (e){
+//     let value = e.target.value
+//     if(value && value.trim().l)
 
-});
+// });
 
 function GetData(){
     fetch("").then(
